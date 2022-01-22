73f9f513f2d5d045eb115144578e7140eebf619d
epiczain1234 <mukatizain@gmail.com>
2022-01-22 12:31:38 -0800

    two candidates stores in mapings

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,10 +2,25 @@ pragma solidity >= 0.4.22;
 
 contract Election {
 
-    string public candidate;
-
-    constructor () public {
-        candidate = "Candidate1";
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
     }
 
+    // Basically as hashmap, we have no way of determining how many candidates there are with just the mapping
+
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
+
+    constructor () public {
+        addCandidate("Zain");
+        addCandidate("The Donald");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
 }
